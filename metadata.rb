13856949e1367b12c14ca0fd1f552765fbfcfaa8
Rajat Vikram Singh <rajats@andrew.cmu.edu>
2016-11-17 20:59:57 -0500

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'jenkins_slave'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures jenkins_slave'
+long_description 'Installs/Configures jenkins_slave'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/jenkins_slave/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/jenkins_slave' if respond_to?(:source_url)
